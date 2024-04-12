78d850d73ca7e38580e600ba3be5701a8aacd271
AlishovZeynal <alishovzeynal@gmail.com>
2024-04-13 02:52:52 +0400

    Tailwind CSS & Sidebar

---
+++ components/layout/Sidebar.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <aside class="w-full h-full relative px-5 py-8 bg-sidebar">
+    <!-- TODO: Add shadecn -->
+    <NuxtLink to="/" class="block mb-10">
+      <NuxtImg src="/logo.svg" alt="Logo" width="100px" class="mx-auto" />
+    </NuxtLink>
+  </aside>
+</template>
+
+<style scoped>
+
+</style>
