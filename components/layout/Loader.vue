6c706452463bd8a927e63cfe58618eb4ce198c6f
AlishovZeynal <alishovzeynal@gmail.com>
2024-04-15 19:32:01 +0400

    Loader & Back-end

---
+++ components/layout/Loader.vue
@@ -0,0 +1,12 @@
+<template>
+  <!-- TODO: Auth -->
+	<div class="wrapper">
+		<NuxtImg src="/loader.svg" alt="" width="220" />
+	</div>
+</template>
+
+<style scoped>
+.wrapper {
+	@apply bg-sidebar flex items-center justify-center w-screen h-screen;
+}
+</style>
