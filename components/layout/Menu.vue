14b51014a8206a94ce943a24abfd962dcb6d298b
AlishovZeynal <alishovzeynal@gmail.com>
2024-04-15 18:25:34 +0400

    Nuxt icon & Sidebar finished

---
+++ components/layout/Menu.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import { MENU_DATA } from './menu.data';
+</script>
+
+<template>
+  <div>
+    <NuxtLink
+      v-for="item in MENU_DATA"
+      :key="item.name"
+      :to="item.url"
+      class="w-full flex items-center py-1 px-3 mb-2.5 rounded-lg hover:bg-gray-700 hover:shadow transition-all"
+    >
+      <Icon 
+        :name="item.icon" 
+        class="mr-3" 
+      />
+      <span>{{ item.name }}</span>
+    </NuxtLink>
+  </div>
+</template>
--- components/layout/Sidebar.vue
+++ components/layout/Sidebar.vue
@@ -1,16 +1,15 @@
-<script setup lang="ts">
-
-</script>
+<script setup lang="ts"></script>
 
 <template>
   <aside class="w-full h-full relative px-5 py-8 bg-sidebar">
     <NuxtLink to="/" class="block mb-10">
       <NuxtImg src="/logo.svg" alt="Logo" width="100px" class="mx-auto" />
     </NuxtLink>
-    <UiButton>Send</UiButton>
+    <button class="absolute top-2 right-3 transition-colors hover:text-primary">
+      <Icon name="line-md:logout" size="20" />
+    </button>
+    <LayoutMenu />
   </aside>
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
